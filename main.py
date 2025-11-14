b34f09fbf07c2be713c23da2106830eb9a0c95f1
domi10s <dominic.schuell@gmx.de>
2025-11-14 16:50:43 +0100

    Integration Folium, Erstellen einer Karte und erste Marker

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # Pruefungsstudienarbeit
 # PSA Alexander Hollenrieder, Thilo Fuhrmann, Dominic Schüll, Tobias Zint
+
 import numpy
-import requests as req     
+import requests as req 
+import folium
          
 koordinaten = {
     "coordinates": [
         [10.314009, 47.716193],     # Kempten
-        [10.642521, 48.061231]      # Türkheim
+        [10.642521, 48.061231],      # Türkheim
         [45.5236, -122.6750]        # Portland   
     ]
 
@@ -18,4 +20,38 @@ Zugangsdaten = {
     'Content-Type': 'application/json; charset=utf-8'
 }
 
+#Start und Zielpunkt definieren
+point_a = [10.314009, 47.716193]
+point_b =  [10.642521, 48.061231]
+
+#Map-Anzeigebereich
+m = folium.Map(location=(10.314009, 47.716193), zoom_start=12)
+
+#Marker für Start und Ziel
+folium.Marker(
+    location = [10.314009, 47.716193],
+    tooltip = "Start",
+    popup = "Kempten",
+    icon = folium.Icon(icon="cloud"),
+).add_to(m)
+
+folium.Marker(
+    location = [10.642521, 48.061231],
+    tooltip = "Click me!",
+    popup = "Ziel",
+    icon = folium.Icon(color="green"),
+).add_to(m)
+
+#Verbinden der beiden Punkte
+folium.PolyLine(
+    locations=[point_a, point_b],
+    color="blue",
+    weight=4,
+    opacity=0.8
+).add_to(m)
+
+
+#Anzeigen/Speichern der Karte
+m
+m.save("meine_karte.html")
 
